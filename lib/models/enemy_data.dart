708b485158920df29fd1d8632f607ea9b5f71381
Davidliew1202 <138994275+Davidliew1202@users.noreply.github.com>
2023-07-08 23:22:04 +0800

    Update enemy_data.dart

--- lib/models/enemy_data.dart
+++ lib/models/enemy_data.dart
@@ -1,19 +1,14 @@
-/// This class represents all the details required
-/// to create an [Enemy] component.
+
 class EnemyData {
-  // Speed of the enemy.
+
   final double speed;
 
-  // Sprite ID from the main sprite sheet.
   final int spriteId;
 
-  // Level of this enemy.
   final int level;
 
-  // Indicates if this enemy can move horizontally.
   final bool hMove;
 
-  // Points gains after destroying this enemy.
   final int killPoint;
 
   const EnemyData({
